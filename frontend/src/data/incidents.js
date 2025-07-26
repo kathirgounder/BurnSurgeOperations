13f6589eb82b72055c30908034f2a5c584697c71
Kathir Gounder <kathirgounder@gmail.com>
2025-07-25 19:27:53 -0700

    Update incidents.js

--- frontend/src/data/incidents.js
+++ frontend/src/data/incidents.js
@@ -1,7 +1,7 @@
 export default [
   {
     id: 'fire‑0725‑glendale',
-    name: 'Glendale Wildland‑Urban Interface Fire',
+    name: 'Glendale Wildland‑Urban Interface Fire Thing',
     lat: 34.2, 
     lon: -118.23,
     datetime: '2025-07-25T15:00:00-07:00',
